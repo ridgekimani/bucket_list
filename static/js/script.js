fb52a21ac39e1d447ec97626f9dacf521e2ea946
Ridge Kimani <ridgekimani@gmail.com>
2017-07-15 12:48:46 +0300

    Added javascript functionality to the login, register and create bucket page

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,128 @@
-var config = window.config = {};
 
-config.ResponsiveBootstrapToolkitVisibilityDivs = {
-    'xs': $('<div class="device-xs 				  hidden-sm-up"></div>'),
-    'sm': $('<div class="device-sm hidden-xs-down hidden-md-up"></div>'),
-    'md': $('<div class="device-md hidden-sm-down hidden-lg-up"></div>'),
-    'lg': $('<div class="device-lg hidden-md-down hidden-xl-up"></div>'),
-    'xl': $('<div class="device-xl hidden-lg-down			  "></div>')
-};
+function signUp() {
+    var username = $("#username").val();
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var confirm_password = $("#confirm_password").val();
 
-ResponsiveBootstrapToolkit.use('Custom', config.ResponsiveBootstrapToolkitVisibilityDivs);
+    if (!username){
+        swal("Error!", "Please enter your username", 'error');
+        return 0;
+    }
+    if (!password){
+        swal("Error!", "Please enter your password", 'error');
+        return 0;
+    }
+    if (!confirm_password){
+        swal("Error!", "Please confirm your password", 'error');
+        return 0;
+    }
+    if (confirm_password !== password){
+        swal("Error!", "Passwords don't match", 'error');
+        return 0;
+    }
+
+    $.ajax({
+        url:'/register/',
+        data: {username: username, password: password, confirm_password: confirm_password},
+        type: 'POST',
+        dataType: 'text',
+        success: function(response){
+            var json = JSON.parse(response);
+            swal('Success!', json.success, 'success');
+            window.location.href = '/create_bucket/';
+
+        },
+        error: function (xhr){
+            var json = JSON.parse(xhr.responseText);
+            swal('Error', json.error, 'error');
+            return 0;
+        }
+
+    })
+
+
+}
+
+function login(){
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    if(!username){
+        swal("Error!", "Please enter your username", "error");
+        return 0;
+    }
+    if(!password){
+        swal("Error!","Please enter your password", 'error');
+        return 0;
+    }
+
+    var data = {
+        password:password,
+        username:username
+    };
+
+    $.ajax({
+        url: '/login/',
+        data: data,
+        type: 'POST',
+        success: function(){
+            window.location.href = '/create_bucket/';
+        },
+        error: function(xhr){
+            var json = JSON.parse(xhr.responseText);
+            swal('Error!', json.error, 'error');
+            return 0;
+        }
+    })
+}
+
+function createBucket(value){
+    var bucket_name = $("#bucket_name").val();
+    var description = $("#description").val();
+
+    if (!bucket_name){
+        swal("Error!", "Please enter the bucket name", 'error');
+        return 0;
+    }
+
+    if (!description){
+        swal("Error!", "Please enter the description", 'error');
+        return 0;
+    }
+
+    $.ajax({
+        url: '/create_bucket',
+        data: {bucket_name:bucket_name, description:description},
+        type: 'POST',
+        success: function(){
+            swal('Success!', 'Bucket list created successfully', 'success');
+            if (value === 1){
+                window.location.href = '/view_buckets';
+            }
+            else if (value === 2){
+                window.location.href = '/create_bucket';
+            }
+        },
+        error: function (xhr){
+            var json = JSON.parse(xhr.responseText);
+            swal('Error!', json.error, 'error');
+            return 0;
+        }
+    })
+}
+
+function addActivity(){
+ swal("Info!", 'Working on it', 'info');
+ return 0;
+}
+
+function viewActivities(){
+ swal("Info!", 'Working on it', 'info');
+ return 0;
+}
+
+function deleteActivity(){
+ swal("Info!", 'Working on it', 'info');
+ return 0;
+}
